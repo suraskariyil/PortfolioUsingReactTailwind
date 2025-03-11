ab68a14f13aeefb6f25b8c6b26eca3d672534443
Suras Kariyil <suraskariyil@gmail.com>
2025-03-11 21:33:31 +0530

    home page and Navbar covered

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import { LoadingScreen } from './components/LoadingScreen'
+import { useState } from 'react'
 import "./index.css"
 import './App.css'
-import { useState } from 'react'
+import { LoadingScreen } from './components/LoadingScreen'
+import { Navbar } from './components/Navbar'
+import { MobileMenu } from './components/MobileMenu'
+import { Home } from './components/sections/Home'
 
 function App() {
-  const [isLoaded ,setIsLoaded]= useState(false)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
     <>
-   {!isLoaded && <LoadingScreen onComplete={()=> setIsLoaded(true)}/>} 
- </>
-  )
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div
+        className={`min-h-screen transition-opacity duration-700 
+      ${isLoaded
+            ? "opacity-100"
+            : "opacity-0"
+          } bg-black text-gray-100`}>
+
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home/>
+
+
+
+      </div>
+    </>
+  );
 }
 
 export default App
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
     const [text,setText]=useState("");
-    const fullText ="<Hellow world/>";
+    const fullText ="<Heyy Guyzzz/>";
 
     useEffect(() => {
         let index =0;
