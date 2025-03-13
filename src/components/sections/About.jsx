b1cf8a5c7b0ed104d270336d12fe4d20176e3e2e
Suras Kariyil <suraskariyil@gmail.com>
2025-03-13 22:16:33 +0530

    projects section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -30,9 +30,10 @@ export const About = () => {
                         <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                             <h3 className="text-xl font-bold mb-4">Frontend</h3>
                             <div className="flex flex-wrap gap-2">
-                                {frontendSkills.map((tech) => (
+                                {frontendSkills.map((tech,key) => (
                                     <span
-                                        className="bg-blue-500/10 text-ble-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    key={key}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
                                     hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
                                         {tech}
                                     </span>
